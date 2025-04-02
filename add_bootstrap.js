5fee161127dde239162ced276e5735aa57663000
Justin Franklin <jfranklin@universalconnectivity.com>
2025-04-02 12:28:23 -0400

    Initial commit

---
+++ add_bootstrap.js
@@ -0,0 +1,22 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ */
+
+define(['N/log', 'N/record'], function(log, record) {
+    function pageInit(context) {
+            try {
+                // load bootstrap css
+                var link = document.createElement("link");
+                link.href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css";
+                link.rel = "stylesheet";
+                link.type = "text/css";
+                document.head.appendChild(link);
+            } catch (e) {
+                log.error('Error in pageInit', e.message);
+            }
+        }
+    return {
+        pageInit: pageInit
+    };
+});
